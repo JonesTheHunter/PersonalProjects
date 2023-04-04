b9666a0f53cbc5878a79122c521d34ef65d9993d
daziki <45572109+daziki@users.noreply.github.com>
2023-04-03 22:48:18 -0400

    Area Calculator
    Using the blueprints provided of the Teotihuacan, this simple program finds the total area, as well as the cost of a floor remodel at 180 pesos per the total area.

---
+++ AreaCalcuator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class AreaCalcuator
+{
+    static double Rect(double length, double width)
+    {
+        return length * width;
+    }
+
+    static double Circle(double radius)
+    {
+        return Math.PI * Math.Pow(radius, 2);
+    }
+
+    static double Trian(double bottom, double height)
+    {
+        double halfFive = 0.5;
+        return halfFive * bottom * height;
+    }
+
+    static double CalCityArea(double Arrectangle, double Arcircle, double Artriangle)
+    {
+        return Arrectangle + Arcircle + Artriangle;
+    }
+
+    static double CalFloCost(double areaCost, double pesoCost = 180)
+    {
+        return areaCost * pesoCost;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Rect(750, 500));
+        Console.WriteLine(Circle(375));
+        Console.WriteLine(Trian(750, 500));
+
+        Console.WriteLine("The Total Area is: " + CalCityArea(Rect(750, 500), Circle(375), Trian(750, 500)));
+        Console.WriteLine($"The total cost of remodeling the city will be: {CalFloCost(CalCityArea(Rect(750, 500), Circle(375), Trian(750, 500)), 200)}");
+    }
+}
